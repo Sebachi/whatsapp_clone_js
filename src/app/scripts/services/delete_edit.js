ddaa463edf6f8d8bd698d013addf22b5f55e0e82
Sebastian Ruiz <sebasruiz15@gmail.com>
2023-06-30 00:16:02 -0500

    merging 29/06/2023

--- src/app/scripts/services/delete_edit.js
+++ src/app/scripts/services/delete_edit.js
@@ -1,19 +1,9 @@
-import { deleteMessage } from "./request";
-import { patchMessage } from "./request";
-import { dropMenu } from "../ui/dropmenu";
 const d = document
 
 const message__container = d.getElementById("message__container")
 
 export const delete_edit = async (e) => {
-    
-   
-
-
-
-
     message__container.removeEventListener('click', handleDelete_edit);
     message__container.addEventListener('click', handleDelete_edit);
-
 }
 
